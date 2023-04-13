487000debb1c54ffa6d524d993773bea714de37e
mattia <mattia@castellani>
2023-04-13 16:56:28 +0200

    norking scanner

--- pancetta.py
+++ pancetta.py
@@ -1,31 +1,51 @@
-"""PyBluez ble example beacon_scan.py"""
 
-from bluetooth.ble import BeaconService
+"""
+Simple BLE forever-scan example, that prints all the detected
+LE advertisement packets, and prints a colored diff of data on data changes.
+"""
+import sys
+import bluetooth._bluetooth as bluez
 
+from bluetooth_utils import (toggle_device,
+                             enable_le_scan, parse_le_advertising_events,
+                             disable_le_scan, raw_packet_to_str)
 
-class Beacon:
+dev_id = 0  # the bluetooth device is hci0
+toggle_device(dev_id, True)
 
-    def __init__(self, data, address):
-        self._uuid = data[0]
-        self._major = data[1]
-        self._minor = data[2]
-        self._power = data[3]
-        self._rssi = data[4]
-        self._address = address
+try:
+    sock = bluez.hci_open_dev(dev_id)
+except:
+    print("Cannot open bluetooth device %i" % dev_id)
+    raise
 
-    def __str__(self):
-        ret = "Beacon: address:{ADDR} uuid:{UUID} major:{MAJOR} " \
-              "minor:{MINOR} txpower:{POWER} rssi:{RSSI}" \
-              .format(ADDR=self._address, UUID=self._uuid, MAJOR=self._major,
-                      MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
-        return ret
+enable_le_scan(sock, filter_duplicates=False)
 
+try:
+    prev_data = None
 
-service = BeaconService()
-devices = service.scan(2)
+    def le_advertise_packet_handler(mac, adv_type, data, rssi):
+        global prev_data
+        data_str = raw_packet_to_str(data)
+        data_wo_rssi = (mac, data_str)
+        print("BLE packet: %s %02x %s %d" % (mac, adv_type, data_str, rssi))
+        if prev_data is not None:
+            if data_wo_rssi != prev_data:
+                # color differences with previous packet data
+                sys.stdout.write(' ' * 35 + 'data_diff=')
+                for c1, c2 in zip(data_str, prev_data[1]):
+                    if c1 != c2:
+                        sys.stdout.write('\033[0;33m' + c1 + '\033[m')
+                    else:
+                        sys.stdout.write(c1)
+                sys.stdout.write('\n')
 
-for address, data in list(devices.items()):
-    b = Beacon(data, address)
-    print(b)
+        prev_data = data_wo_rssi
 
-print("Done.")
+    # Blocking call (the given handler will be called each time a new LE
+    # advertisement packet is detected)
+    parse_le_advertising_events(sock,
+                                handler=le_advertise_packet_handler,
+                                debug=False)
+except KeyboardInterrupt:
+    disable_le_scan(sock)
